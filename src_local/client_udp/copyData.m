93ccd7b5807080334c1338f5925f94bf2ab9dd9c
YecidMorenoUSP <yecidmoreno@usp.br>
2023-10-25 12:24:14 -0300

    plot velocity

--- src_local/client_udp/copyData.m
+++ src_local/client_udp/copyData.m
@@ -3,7 +3,7 @@ clearvars
 
 !scp -r debian@192.168.0.232:/home/debian/out .
 %%
-f = "out/_threadControl.dat";
+f = "out/file_Knee_L.dat";
 
 d = importdata(f,'\t');
 
@@ -20,24 +20,37 @@ for idx = 1:numel(d.colheaders)
 end
 
 t = data.time;
+tau_cal = (data.knee_pos_in/150 - data.knee_pos_out)*104;
 
 figure(200), clf
 
-subplot(221)
+subplot(321)
+hold on
 plot(t,data.knee_pos_in)
+plot(t,data.knee_pos_out*150)
 ylabel("position [u]")
 
-subplot(222)
+subplot(322)
 plot(t,data.knee_pos_out)
 ylabel("position [u]")
 
-subplot(223)
+subplot(323)
+hold on
+plot(t,data.knee_vel_d)
 plot(t,data.knee_vel_in)
+legend("omega_d","omega")
 ylabel("velocity [u/s]")
 
-subplot(224)
+subplot(324)
 plot(t,data.knee_vel_out)
 ylabel("velocity [u/s]")
+
+subplot(325)
+hold on
+plot(t,data.knee_tau_d)
+plot(t,data.knee_tau_l)
+legend("tau_d","tau")
+ylabel("tau [u/s]")
 
 for ax = findall(gcf,'type','axes')'
     axes(ax)
